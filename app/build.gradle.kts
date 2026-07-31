c4aa31eabf1947156901a457819e80b3161b2afa
logie-github <177881302+logie-github@users.noreply.github.com>
2026-07-30 20:00:28 -0400

    Reduce id/vitals column width by 10%

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,8 +39,8 @@ android {
         applicationId = "com.logie.pgearhs"
         minSdk = 26
         targetSdk = 35
-        versionCode = 82
-        versionName = "1.0.82"
+        versionCode = 83
+        versionName = "1.0.83"
 
         buildConfigField("String", "GIT_COMMIT", "\"$gitCommit\"")
 
