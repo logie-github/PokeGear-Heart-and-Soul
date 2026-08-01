ee2008bad33ba21c299c0c90a43f43c948b5ab65
logie-github <177881302+logie-github@users.noreply.github.com>
2026-08-01 17:49:33 -0400

    Strip font leading from description text so vertical padding visually matches horizontal

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,8 +39,8 @@ android {
         applicationId = "com.logie.pgearhs"
         minSdk = 26
         targetSdk = 35
-        versionCode = 100
-        versionName = "1.0.100"
+        versionCode = 101
+        versionName = "1.0.101"
 
         buildConfigField("String", "GIT_COMMIT", "\"$gitCommit\"")
 
